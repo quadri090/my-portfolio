b8ba70483ef6aaa0a15b5fe3ca61010d21e88b95
quadri090 <quadri090@gmail.com>
2023-02-25 21:26:56 +0100

    added aos. 25th feb 23

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    // colors: {
+    //   custom1: '#FF8303',
+    //   custom2: '#1F1E1B',
+    //   custom2: '#21201D',
+
+    // },
+    
+    extend: {
+    },
+    fontFamily: {
+    // 'inconsolata': ['roboto', 'san-serif']
+    display: ["Satisfy", "Roboto"],
+  }
+  },
+  // plugins: [
+  //   require('@tailwindcss/typography'),
+  //   require('@tailwindcss/forms'),
+  //   require('@tailwindcss/aspect-ratio')
+  // ],
+}
